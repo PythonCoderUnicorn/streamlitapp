3a05b62be94bad15f91ad21c38f1233adead9bc2
PythonCoderUnicorn <neptunium0237@gmail.com>
2022-12-22 20:36:52 -0700

    widgets

--- hello.py
+++ hello.py
@@ -41,3 +41,23 @@ st.write(n)
 st.markdown('Streamlit is **_really_ cool**.')
 st.markdown(' This text is :red[colored red], and this is **:blue[colored]** and bold.')
 st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
+
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+
+
+
+import streamlit as st
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
